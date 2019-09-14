911982a3cf8e29657acc37489e8f28775bbe6d41
Jason R. Coombs <jaraco@jaraco.com>
2019-09-14 20:18:33 +0200

    Clean up serving script

--- serve.py
+++ serve.py
@@ -1,17 +1,31 @@
-import os.path
+import os
+
 import cherrypy
 from cherrypy.lib.static import serve_file
+
 
 class Site(object):
     @cherrypy.expose
     def index(self):
         return serve_file(os.path.join(os.getcwd(), 'index.html'))
 
-if __name__ == '__main__':
-    cherrypy.quickstart(Site(), '', {'/images': {'tools.staticdir.on': True,
-                                                 'tools.staticdir.dir': os.path.join(os.getcwd(), 'images')},
-                                     '/js': {'tools.staticdir.on': True,
-                                             'tools.staticdir.dir': os.path.join(os.getcwd(), 'js')},
-                                     '/css': {'tools.staticdir.on': True,
-                                              'tools.staticdir.dir': os.path.join(os.getcwd(), 'css')}})
-    
+    @classmethod
+    def run(cls):
+        config = {
+            '/images': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': os.path.join(os.getcwd(), 'images'),
+            },
+            '/js': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': os.path.join(os.getcwd(), 'js'),
+            },
+            '/css': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': os.path.join(os.getcwd(), 'css'),
+            },
+        }
+        cherrypy.quickstart(cls(), config=config)
+
+
+__name__ == "__main__" and Site.run()
